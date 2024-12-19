c99861a98d1285c6897dc549f8c502c79a4dc533
shyam-kumar8848 <113445741+shyam-kumar8848@users.noreply.github.com>
2024-12-19 19:08:03 +0530

    feat: attendabce biomatric

--- biometric/biometric/doctype/attendance_biometric/attendance_biometric.py
+++ biometric/biometric/doctype/attendance_biometric/attendance_biometric.py
@@ -1,36 +1,46 @@
 import frappe
 from frappe.model.document import Document
 from frappe.utils import now_datetime
+from datetime import datetime
 
 class AttendanceBiometric(Document):
     def after_insert(self):
         settings = frappe.get_single("Biometric Settings")
+        
+        if not hasattr(self, 'employeeid'):
+            frappe.throw(_("Employee ID is missing from Attendance Biometric document"))
+        
+        employee_ids = frappe.get_all("Employee", filters={"attendance_device_id": self.employeeid}, fields=["name", "attendance_device_id"])
 
-        if settings.employee_checkin:
-            frappe.get_doc({
-                "doctype": "Employee Checkin",
-                "employee": self.attendancelogid, 
-                "time": now_datetime(),
-                "log_type": "IN",
-                "attendance_biometric": self.name
-            }).insert(ignore_permissions=True)
+        for employee in employee_ids:
+            intime = datetime.strptime(self.intime, "%Y-%m-%d %H:%M:%S")
+            log_type = "IN" if intime.hour < 12 else "OUT"
 
-        if settings.attendance:
-            frappe.get_doc({
-                "doctype": "Attendance",
-                "employee": self.attendancelogid,
-                "attendance_date": now_datetime().date(),
-                "status": "Present",  
-                "attendance_biometric": self.name
-            }).insert(ignore_permissions=True)
+            if settings.employee_checkin:
+                frappe.get_doc({
+                    "doctype": "Employee Checkin",
+                    "employee": employee['name'],
+                    "time": self.intime,
+                    "log_type": log_type,
+                    "attendance_biometric": self.name
+                }).insert(ignore_permissions=True)
 
-        if settings.attendance_request:
-            frappe.get_doc({
-                "doctype": "Attendance Request",
-                "employee": self.attendancelogid,
-                "from_date": now_datetime().date(),
-                "to_date": now_datetime().date(),
-                "reason": "Auto-created by Attendance Biometric",
-                "status": "Open",
-                "attendance_biometric": self.name
-            }).insert(ignore_permissions=True)
+            if settings.attendance:
+                frappe.get_doc({
+                    "doctype": "Attendance",
+                    "employee": employee['name'],
+                    "attendance_date": self.intime,
+                    "status": "Present",  
+                    "attendance_biometric": self.name
+                }).insert(ignore_permissions=True)
+
+            if settings.attendance_request:
+                frappe.get_doc({
+                    "doctype": "Attendance Request",
+                    "employee": employee['name'],
+                    "from_date": self.intime,
+                    "to_date": self.outtime,
+                    "reason": "Auto-created by Attendance Biometric",
+                    "status": "Open",
+                    "attendance_biometric": self.name
+                }).insert(ignore_permissions=True)
