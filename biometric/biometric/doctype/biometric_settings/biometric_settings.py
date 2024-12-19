e04e540ff0e9c3f08ee04d5c9e2710615e829042
shyam-kumar8848 <113445741+shyam-kumar8848@users.noreply.github.com>
2024-12-19 11:47:50 +0530

    feat: attendabce biometric

--- biometric/biometric/doctype/biometric_settings/biometric_settings.py
+++ biometric/biometric/doctype/biometric_settings/biometric_settings.py
@@ -1,9 +1,19 @@
-# Copyright (c) 2024, 8848 Digital and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class BiometricSettings(Document):
-	pass
+    def validate(self):
+        self.validate_api_credentials()
+
+    def validate_api_credentials(self):
+        if not self.api_key or not self.api_secret:
+            frappe.throw("API Key and API Secret are mandatory.")
+
+        # Check if User with the provided API Key and Secret exists
+        user = frappe.db.exists("User", {
+            "api_key": self.api_key,
+            "api_secret": self.api_secret
+        })
+
+        if not user:
+            frappe.throw("Invalid API Key or API Secret. Please check your credentials.")
