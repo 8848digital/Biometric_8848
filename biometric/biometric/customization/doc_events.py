79852f1d478bd7bdcc85be88d3ee2998bed74178
shyam-kumar8848 <shyamkumar@8848digital.com>
2025-01-07 14:01:11 +0530

    feat: Hooks File

--- biometric/biometric/customization/doc_events.py
+++ biometric/biometric/customization/doc_events.py
@@ -7,7 +7,6 @@ def before_save(doc ,method = None):
 def update_start_and_end_time(doc):
     if doc.custom_created_by_attendance_biometric == 1:
         if doc.custom_log_type == "IN":
-            # frappe.db.set_value(doc.doctype,doc.name,"end_time",None)
             doc.end_time = None
             
         
