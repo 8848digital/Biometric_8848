a20d5112c95f4c1e4208fc42b5c5bc81a45d2ed1
shyam-kumar8848 <shyamkumar@8848digital.com>
2025-01-07 13:56:18 +0530

    feat: report

--- biometric/biometric/customization/doc_events.py
+++ biometric/biometric/customization/doc_events.py
@@ -4,7 +4,6 @@ import frappe
 def before_save(doc ,method = None):
     update_start_and_end_time(doc)
     
-
 def update_start_and_end_time(doc):
     if doc.custom_created_by_attendance_biometric == 1:
         if doc.custom_log_type == "IN":
